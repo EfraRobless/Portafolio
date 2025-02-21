e1b790621e2ca819e096a4f9800d7b42cbc0552c
EfraRobless <efrarobles01@gmail.com>
2025-02-20 20:23:31 -0500

    Portafolio modificado

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base:"https://EfraRobless.github.io/Portafolio",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
